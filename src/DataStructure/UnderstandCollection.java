a8a4f98dfa6e209105546758f70d631939467c32
Haorui Yang <83082501+Jasper-Rui@users.noreply.github.com>
2022-01-10 23:58:23 -0400

    update

--- src/DataStructure/UnderstandCollection.java
+++ src/DataStructure/UnderstandCollection.java
@@ -1,7 +1,6 @@
 package DataStructure;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,7 +11,38 @@ import java.util.Collection;
  * Description:
  */
 public class UnderstandCollection {
+
     public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Jasper", "No.1");
+        map.put("Haorui", "No.2");
+        map.put("Yang", "No.3");
+        String result = map.get("Jasper");
+        System.out.println(result);
+
+        String result2 = map.get("Jasperrr");
+        System.out.println(result2);
+        //if the value corresponding to key doesn't;t exist, it will return null
+
+        String result3 = map.getOrDefault("Jasperrr", "heihei");
+        System.out.println(result3);
+
+        boolean flg = map.containsKey("haorui");
+        boolean flg1 = map.containsKey("Haorui");
+        System.out.println(flg);
+        System.out.println(flg1);
+        System.out.println(map);
+
+        Set<Map.Entry<String, String>> entrySet = map.entrySet();
+        for (Map.Entry<String, String> ebtry : entrySet) {
+            System.out.println( "Key is " + ebtry.getKey() + " value is " + ebtry.getValue());
+        }
+
+    }
+
+
+
+    public static void main1(String[] args) {
 
         Collection<String> collection = new ArrayList<>();
         //generics
@@ -29,5 +59,6 @@ public class UnderstandCollection {
         System.out.println(collection1);
         collection1.clear();
         System.out.println(collection1);
+
     }
 }
